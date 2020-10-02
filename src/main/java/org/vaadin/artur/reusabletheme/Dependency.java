9d8ae5f213b961d077ce554764a5649c875d4cfc
Artur Signell <artur@vaadin.com>
2020-10-02 22:14:40 +0300

    Example of a reusable theme

---
+++ src/main/java/org/vaadin/artur/reusabletheme/Dependency.java
@@ -0,0 +1,8 @@
+package org.vaadin.artur.reusabletheme;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@NpmPackage(value = "@fortawesome/fontawesome-free", version = "5.15.0")
+public class Dependency {
+
+}
